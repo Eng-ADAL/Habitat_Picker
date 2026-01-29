4cc647e5587035bfce2742b37f82dd71ffea14ad
eng-adal <186938423+Eng-ADAL@users.noreply.github.com>
2026-01-29 23:37:37 +0000

    feat: add path-based PPD CSV extraction with schema validation

--- src/app.py
+++ src/app.py
@@ -4,12 +4,16 @@
 Store Habitat_Picker main app structure
 """
 
-from extract import extract_ppd_csv
+from pathlib    import Path
 
-def main():
-    df = extract_ppd_csv()
+from extract    import extract_ppd_csv
+from config     import DATA_RAW_DIR
+
+def main(input_path: Path):
+    df = extract_ppd_csv(input_path)
+    # df = transform...
     print(df.head())
 
 if __name__ == "__main__":
-    main()
+    main(DATA_RAW_DIR / "sample_data.csv")
 
--- src/extract.py
+++ src/extract.py
@@ -5,9 +5,39 @@ Store Habitat_Picker extract code for csv file
 """
 
 import pandas as pd
-from config import DATA_RAW_DIR
+from pathlib    import Path
 
-def extract_ppd_csv(filename: str = "sample_data.csv") -> pd.DataFrame:
-    path = DATA_RAW_DIR / filename
-    return pd.read_csv(path)
+# pp-monthly.csv columns
+PPD_COLUMNS = [
+    "transaction_id",
+    "price",
+    "date_of_transfer",
+    "postcode",
+    "property_type",
+    "new_build_flag",
+    "tenure_type",
+    "paon",
+    "saon",
+    "street",
+    "locality",
+    "town_city",
+    "district",
+    "county",
+    "ppd_category",
+    "record_status",
+]
+
+
+def extract_ppd_csv(path: Path) -> pd.DataFrame:
+    if not path.exists():
+        # log
+        raise FileNotFoundError(f"PPD source file not found {path}")
+
+    df = pd.read_csv(path, header=None)
+
+    if df.shape[1] != len(PPD_COLUMNS):
+        raise ValueError(f"Invalid column count! Expected: {len(PPD_COLUMNS)}, got: {df.shape[1]}")
+
+    df.columns = PPD_COLUMNS
+    return df
 
